2737eb07378e659535ec12ef6c206849c91088c7
Julia <juliatangwc@gmail.com>
2022-03-26 23:51:03 +0000

    Set up initial routes

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-"""Server for movie ratings app."""
+"""Server for reservations app."""
 
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 from model import connect_to_db, db
-import crud
+import helper
 
 from jinja2 import StrictUndefined
 
@@ -16,102 +16,6 @@ app.jinja_env.undefined = StrictUndefined
 def show_homepage():
     """Show homepage"""
     return render_template("homepage.html")
-
-
-@app.route("/movies")
-def show_movies():
-    """Show all movies"""
-
-    movies = crud.all_movies_list()
-
-    return render_template("movies.html", movies = movies)
-
-@app.route("/movies/<movie_id>")
-def show_movie(movie_id):
-    """Show details on a particular movie."""
-    
-    movie = crud.get_movie_by_id(movie_id)
-
-    return render_template("movie_details.html", movie=movie)
-
-# This did not work but I wonder if it's possible.
-# @app.route("/movies/<movie_id>", methods=["POST"])
-# def rate_a_movie(movie_id):
-#     """Allow users to submit a rating to a movie."""
-#     if session.get('pkey', None) is None:
-#         flash ("Please log in to rate a movie.")
-#     else:
-#         user_id = session['pkey']
-#         score = int(request.form.get("rating"))
-#         user = crud.get_user_by_id(user_id)
-#         movie = crud.get_movie_by_id(movie_id)
-
-#         if score not in range(0,6):
-#             flash("Invalid entry. Try again.")
-#         else:
-#             new_rating = crud.rate_a_movie(user, movie, score)
-#             db.session.add(new_rating)
-#             db.session.commit()
-#             flash ("Rating added.")
-
-#     return redirect(request.referrer)
-
-@app.route("/rating", methods=["POST"])
-def rate_a_movie():
-    """Allow users to submit a rating to a movie."""
-    if session.get('pkey', None) is None:
-        flash ("Please log in to rate a movie.")
-    else:
-        user_id = session['pkey']
-        movie_id = request.form.get("movie_id")
-        score = int(request.form.get("rating"))
-        user = crud.get_user_by_id(user_id)
-        movie = crud.get_movie_by_id(movie_id)
-
-        if score not in range(0,6):
-            flash("Invalid entry. Try again.")
-        else:
-            new_rating = crud.rate_a_movie(user, movie, score)
-            db.session.add(new_rating)
-            db.session.commit()
-            flash ("Rating added.")
-
-    return redirect(request.referrer)
-
-@app.route("/users")
-def show_users():
-    """Show all users"""
-
-    users = crud.all_users_list()
-
-    return render_template("users.html", users = users)
-
-@app.route("/users/<user_id>")
-def show_user(user_id):
-    """Show details on a particular user."""
-    
-    user = crud.get_user_by_id(user_id)
-
-    return render_template("user_details.html", user=user)
-
-
-@app.route("/users", methods=["POST"])
-def register_user():
-    """Create a new user."""
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    user_exist = crud.get_user_by_email(email)
-
-    if user_exist:
-        flash ("This email is already registered on our website. Please log in.")
-    else:
-        user = crud.create_user(email, password)
-        db.session.add(user)
-        db.session.commit()
-        flash ("Account created. Please log in.")
-    
-    return redirect ("/")
 
 @app.route("/login", methods=["POST"])
 def log_in():
@@ -132,9 +36,30 @@ def log_in():
     else:
         flash ("No match for email entered. Please create an account.")
     
-    return redirect ("/")
+    return redirect ("/reservation-form")
 
+@app.route("/reservation-form")
+def show_reservation_form():
+    """Show reservation form"""
+    return render_template("reservation-form.html")
 
+@app.route("/available")
+def show_available slots():
+    """Show all available slots according to criteria given."""
+    return render_template("available-timeslots.html")
+
+@app.route("/reserve", methods=["POST"])
+def make_reservation():
+    """Make reservation. Assign a user to a slot."""
+    pass
+
+@app.route("/user/<user_id>")
+def show_reservations_by_user():
+    """Show all reservations made by a user."""
+
+    reservations = helper.all_reservation_list()
+
+    return render_template("user.html", reservations = reservations)
 
 
 if __name__ == "__main__":
